e690806e2fcab69df2b345d372d4280859821f8a
Lucas Villela Neder Issa <lucas.issa@gmail.com>
2017-07-25 20:10:48 -0300

    Running build.

--- dist/components/__tests__/sticky-stack-context-test.js
+++ dist/components/__tests__/sticky-stack-context-test.js
@@ -36,51 +36,128 @@ describe('StickyStackContext', function () {
   });
 
   it('registers an item', function () {
-    stickyStackContext._register(0, 10, 10);
+    var component = {
+      domRef2: {
+        offsetTop: 0,
+        offsetHeight: 15,
+        style: {
+          position: 'relative'
+        }
+      }
+    };
+    var level = 0;
+    var registration = stickyStackContext._register(component, level);
 
-    expect(stickyStackContext.state.items.length).toBe(1);
+    expect(stickyStackContext.components.length).toBe(1);
+    expect(stickyStackContext.components[level][registration.registrationId].registrationId).toBe(1);
   });
 
-  it('calculates the styles (static)', function () {
-    stickyStackContext._register(0, 10, 10);
+  it('calculates the styles (relative)', function () {
+    var component = {
+      domRef2: {
+        offsetTop: 1,
+        offsetHeight: 15,
+        style: {
+          position: 'relative'
+        }
+      },
+      forceUpdate: function forceUpdate() {}
+    };
+    var level = 0;
+    var registration = stickyStackContext._register(component, level);
+    window.pageYOffset = 0;
     stickyStackContext._calculateStyles();
 
-    expect(stickyStackContext.state.styles[0]).toEqual({
-      position: 'static'
-    });
+    expect(stickyStackContext.styles[level][registration.registrationId].position).toEqual('relative');
   });
 
   it('calculates the styles (fixed)', function () {
-    stickyStackContext._register(0, 10, 10);
+    var component = {
+      domRef2: {
+        offsetTop: 5,
+        offsetHeight: 15,
+        style: {
+          position: 'static'
+        }
+      },
+      forceUpdate: function forceUpdate() {}
+    };
+    var level = 0;
+    var registration = stickyStackContext._register(component, level);
     window.pageYOffset = 11;
     stickyStackContext._calculateStyles();
 
-    expect(stickyStackContext.state.styles[0]).toEqual({
+    expect(stickyStackContext.styles[level][registration.registrationId]).toEqual({
       position: 'fixed',
       top: 0,
-      width: '100%'
+      width: 'calc(100% - 0px)',
+      zIndex: 11
     });
   });
 
   it('calculates the styles (stacked)', function () {
-    stickyStackContext._register(0, 10, 10);
-    stickyStackContext._register(1, 10, 10);
-    window.pageYOffset = 11;
+    var component01 = {
+      domRef2: {
+        offsetTop: 0,
+        offsetHeight: 10,
+        style: {
+          position: 'relative'
+        }
+      },
+      forceUpdate: function forceUpdate() {}
+    };
+    var level01 = 0;
+    var registration01 = stickyStackContext._register(component01, level01);
+
+    var component02 = {
+      domRef2: {
+        offsetTop: 20,
+        offsetHeight: 15,
+        style: {
+          position: 'relative'
+        }
+      },
+      forceUpdate: function forceUpdate() {}
+    };
+    var level02 = 1;
+    var registration02 = stickyStackContext._register(component02, level02);
+
+    window.pageYOffset = 25;
     stickyStackContext._calculateStyles();
 
-    expect(stickyStackContext.state.styles[1]).toEqual({
+    expect(stickyStackContext.styles[level01][registration01.registrationId]).toEqual({
+      position: 'fixed',
+      top: 0,
+      width: 'calc(100% - 0px)',
+      zIndex: 13
+    });
+
+    expect(stickyStackContext.styles[level02][registration02.registrationId]).toEqual({
       position: 'fixed',
       top: 10,
-      width: '100%'
+      width: 'calc(100% - 0px)',
+      zIndex: 12
     });
   });
 
   it('returns the style', function () {
-    stickyStackContext._register(0, 10, 10);
+    var component = {
+      domRef2: {
+        offsetTop: 10,
+        offsetHeight: 15,
+        style: {
+          position: 'static'
+        }
+      },
+      forceUpdate: function forceUpdate() {}
+    };
+    var level = 0;
+    var registration = stickyStackContext._register(component, level);
     stickyStackContext._calculateStyles();
 
-    expect(stickyStackContext._getStyle(0)).toEqual({
-      position: 'static'
+    expect(stickyStackContext.styles[level][registration.registrationId]).toEqual({
+      position: 'relative',
+      zIndex: 10
     });
   });
 });
