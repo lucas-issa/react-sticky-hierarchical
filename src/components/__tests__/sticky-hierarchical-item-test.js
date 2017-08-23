6426fd1db0762ac8e9311d22183da9d8a0beca44
Lucas Villela Neder Issa <lucas.issa@gmail.com>
2017-08-22 22:01:55 -0300

    Fixing unit test.

--- src/components/__tests__/sticky-hierarchical-item-test.js
+++ src/components/__tests__/sticky-hierarchical-item-test.js
@@ -21,7 +21,7 @@ describe('StickyHierarchicalItem', () => {
       },
       getChildContext() {
         return {
-          getStyle: () => {},
+          getStyle: () => ({}),
           register,
         };
       },
